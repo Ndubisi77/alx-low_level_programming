b6dafe3c81e1991f673f8871eee60ed9a3e6da87
Ndubisi77 <ndubisichidubem@gmail.com>
2022-10-17 12:18:39 -0700

    okk

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,24 +11,24 @@ int main(void)
 	{
 		for (j = k; j <= '9'; j++)
 		{
-				if (i != j)
-				{
-				putchar(i);
-				putchar(j);
-				}
-				if (i == j)
-				{
-				continue;
-				}
-				if (i == '8' && j == '9')
-				{
-				break;
-				}
-				else
-				{
-				putchar(',');
-				putchar(' ');
-				}
+			if (i != j)
+			{
+			putchar(i);
+			putchar(j);
+			}
+			if (i == j)
+			{
+			continue;
+			}
+			if (i == '8' && j == '9')
+			{
+			break;
+			}
+			else
+			{
+			putchar(',');
+			putchar(' ');
+			}
 		}
 		k++;
 	}
