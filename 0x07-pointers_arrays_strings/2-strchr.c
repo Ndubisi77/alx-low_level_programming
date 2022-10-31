8c200e092ac37048598d5c86c08a250fab3c0c0c
Ndubisi77 <ndubisichidubem@gmail.com>
2022-10-31 16:56:30 -0700

    ikm

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,22 +6,14 @@
  * @c: character.
  * Return: the pointer to the first occurrence of the character c.
  */
-
 char *_strchr(char *s, char c)
-
 {
-
 	unsigned int i = 0;
 
 	for (; *(s + i) != '\0'; i++)
-
 		if (*(s + i) == c)
-		return (s + i);
-
-		if (*(s + i) == c)
-
 			return (s + i);
-
-					return ('\0');
-
+	if (*(s + i) == c)
+		return (s + i);
+	return ('\0');
 }
